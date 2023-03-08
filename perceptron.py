301072a4261a663183e4a7eb60a8385c3847122f
skosachiov <skosachiov@gmail.com>
2023-03-08 23:01:41 +0300

    add some comments

--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,5 @@
 import numpy as np
 import random
-import time
-
 
 class Perceptron:
 
@@ -16,7 +14,7 @@ class Perceptron:
 
     # sigmoid
     def F(self, x): return 1.0 / (1.0 + np.exp(-self.alpha*x))
-    
+
     # sigmoid derivative
     def dF(self, x): return 2 * self.alpha * self.F(x) * (1.0 - self.F(x))
 
@@ -27,13 +25,13 @@ class Perceptron:
         return self.o[self.L - 1]
 
     def backprop(self, t):
-        e = self.o[self.L - 1] - self.F(np.array(t).flatten())
-        loss = 0.5 * np.sum(e**2)
+        err = self.o[self.L - 1] - self.F(np.array(t).flatten())
+        loss = 0.5 * np.sum(err**2)
         for n in reversed(range(self.L - 1)):
-            d = self.dF(self.o[n+1]) * e
+            d = self.dF(self.o[n+1]) * err
             dW = -self.eta * np.outer(self.o[n], d)
             self.W[n] += dW
-            e = self.W[n] @ d
+            err = self.W[n] @ d
         return loss
 
     def training(self, x, t):
@@ -46,14 +44,15 @@ class Perceptron:
     def load_model():
         pass
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
 
+    # data preparation for training
     z = [ [1,1,1], [0,0,1], [1,1,1], [1,1,1], [1,0,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1],
           [1,0,1], [0,0,1], [0,0,1], [0,0,1], [1,0,1], [1,0,0], [1,0,0], [0,0,1], [1,0,1], [1,0,1],
           [1,0,1], [0,0,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1], [1,1,1], [0,0,1], [1,1,1], [1,1,1],
           [1,0,1], [0,0,1], [1,0,0], [0,0,1], [0,0,1], [0,0,1], [1,0,1], [0,0,1], [1,0,1], [0,0,1],
           [1,1,1], [0,0,1], [1,1,1], [1,1,1], [0,0,1], [1,1,1], [1,1,1], [0,0,1], [1,1,1], [1,1,1] ]
-    a = []
+    a = [] # list of tuples
     b = []
     for i in range(10):
         s = []
@@ -63,23 +62,28 @@ if __name__ == "__main__":
             s.append(z[i + 10*j])
         a.append((s, b))
 
-    layers = [15, 30, 30, 10]
+    # perceptron parameters
+    layers = [15, 30, 30, 10] # input layer (3*5), two hidden layers (30, 30), output layer (10)
     alpha = 2.0
     eta = 0.005
     epoch = 1000000
 
+    # initialization
     p = Perceptron(layers, alpha, eta)
 
+    # you can use fewer digits, for example, items = 5
     items = len(a)
 
-    for i in range(epoch * items):
-        k = random.randrange(items)
-        e = p.training(a[k][0], a[k][1])
-        if i%1000 == 0: 
-            print("LOSS =", e)
+    # learning process
+    for i in range(epoch*items):
+        r = random.randrange(items)
+        err = p.training(a[r][0], a[r][1])
+        if i % 1000 == 0:
+            print("LOSS =", err)
             print("DONE =", 100 * i/(epoch*items), "%" )
             print()
 
+    # examination
     np.set_printoptions(precision=3)
     for i in range(items):
         print("TEST   =", p.F(np.array(a[i][1])))
